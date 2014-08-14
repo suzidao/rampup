fb766b4bc9037ef05046369f035bf2d4ab7d9e0a
Susanne Dao <susanne.dao@gmail.com>
2014-08-13 20:12:20 -0400

    week 2 homework

--- practice.rb
+++ practice.rb
@@ -1,5 +1,13 @@
 PRICE_PER_OUNCE = 1336
 
-def price_in_gold(ounces)
-  ounces * PRICE_PER_OUNCE
+def price_of_gold_from_pounds(pounds)
+  @pounds = pounds
+  @ounces = pounds * 16
+  def price_of_gold(ounces)
+    @price = ounces * PRICE_PER_OUNCE
+    puts "The price of #{@pounds} pounds of gold is $#{@price}."
+  end
+  price_of_gold(@ounces)
 end
+
+# price_of_gold_from_pounds(3)
